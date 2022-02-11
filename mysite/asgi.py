20aac6d1c0cfcbfe3cfca879bfbaa76f316bf7b6
Eagle Code <tomoki.takaba@outlook.jp>
2022-02-11 17:13:15 +0900

    no message

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,6 +1,9 @@
 import os
 import django
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
 import blog.routing
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -8,8 +11,6 @@ from channels.security.websocket import AllowedHostsOriginValidator
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
    'http': get_asgi_application(),
