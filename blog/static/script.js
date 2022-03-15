8975aa1281cc91d2e09f6abffba9577112d3f719
Eagle Code <tomoki.takaba@outlook.jp>
2022-03-15 17:50:04 +0900

    no message

--- blog/static/script.js
+++ blog/static/script.js
@@ -136,7 +136,7 @@ editor.on('change',()=>{
   var a_list=ele.getElementsByTagName("a")
   func_list=[]
   for(var a of a_list){
-    console.log(a.parentNode.classList)
+    console.log(a.parentNode.classList.contains("twitter-tweet"))
     if(a.parentNode.classList.contains("twitter-tweet")){
       func_list.push(new Promise((resolve,reject) => {resolve()}))
     }
